d4f3c5f47a7cbac985b5c371604568d7e4eca66e
Lucas ResendeF <lucasresendef@hotmail.com>
2021-01-28 14:38:25 -0300

    Projeto em JS de Urna Eletronica

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'13145',
+                name:'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'13145.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'14524',
+                name:'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'14524.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'11',
+                name:'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'11.png', legenda:'Prefeito'},
+                    {url:'11-1.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'11',
+                name:'Zulano',
+                partido: 'DEFG',
+                vice: 'ZUL',
+                fotos:[
+                    {url:'14.png', legenda:'Prefeito'},
+                    {url:'14-1.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
